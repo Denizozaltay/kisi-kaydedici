e9dadda9b3b778f3d38081e356bc62bb789d48fc
Deniz Özaltay <denizozaltay2003@hotmail.com>
2023-02-06 10:17:28 +0300

    Yaş sistemi geldi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 if not os.path.exists("Kişiler.xlsx"):
-    df = pd.DataFrame(columns=["İsim","Soyisim"])
+    df = pd.DataFrame(columns=["İsim","Soyisim","Yaş"])
     df.to_excel("Kişiler.xlsx", index=False)
 
 df = pd.read_excel("Kişiler.xlsx")
@@ -13,15 +13,16 @@ while True:
     if secim.lower() == "add":
         isim = input("İsim: ")
         soyisim = input("Soyisim: ")
+        yas = input("Yaş: ")
 
         isim = isim.capitalize()
         soyisim = soyisim.capitalize()
 
-        df = pd.concat([df, pd.DataFrame({"İsim" : [isim], "Soyisim" : [soyisim]})], ignore_index=True)
+        df = pd.concat([df, pd.DataFrame({"İsim" : [isim], "Soyisim" : [soyisim], "Yaş" : [yas]})], ignore_index=True)
 
         df.to_excel("Kişiler.xlsx", index=False)
     elif secim.lower() == "clear":
-        df = pd.DataFrame(columns=["İsim", "Soyisim"])
+        df = pd.DataFrame(columns=["İsim", "Soyisim","Yaş"])
         df.to_excel("Kişiler.xlsx", index=False)
     elif secim.lower() == "exit":
         break
